55a7cc54d92b1bb4bce95f4b5289d97ff6269b22
Hamilton Lumati <hamiltonlumati@gmail.com>
2024-07-23 11:11:47 +0100

    tailwind configuration and template cleaning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/App.jsx",
+    "./src/main.jsx"
+  ],
   theme: {
+    colors: {
+      'background': '#f0f2f5',
+      'blue': '#0866ff',
+      'white': '#FFFFFF',
+      'form-border': '#1d2129',
+      'green': '#42b72a',
+      'linebreak': '#1c1e21',
+      'fontColor': '#1c1e21',
+      'darkBlue': '#1e3a8a',
+      'darkGreen': '#064e3b',
+      'blueRing': '#bfdbfe'
+    },
     extend: {},
   },
   plugins: [],
 }
-
